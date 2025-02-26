a8d2a39d7d37dd03b15e4ee662a0285a4d0434d6
OgbuEsther <ogbuuzoma413@gmail.com>
2025-02-26 18:53:24 +0100

    added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Header from './header/Header'
 import Hero from './hero/Hero'
 import Content from './content/Content'
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            <h3>get <span>Linked</span></h3>
-        </div>
-        <div>
-            <nav>Home</nav>
-            <nav>About</nav>
-            <nav>Contact</nav>
+    
 
-            <button style={{width : '100px' , height : '50px' , backgroundColor : '#E52EE3'}}>Register</button>
+   <div>
+     <div style={{width : '100vw' , height : '70px' , backgroundColor : '#150E28' , display : 'flex' , justifyContent: 'space-between' , alignItems: 'center' , color : 'white' , fontWeight : '500'}}>
+        <div  style={{ marginLeft : '10px' }}>
+            <h3>get<span style={{color : '#E52EE3'}}>Linked</span></h3>
+        </div>
+        <div style={{ display : 'flex' , justifyContent: 'space-between' , alignItems: 'center'}}>
+            <nav  style={{fontSize: '12px' , marginRight : '10px' }}>Home</nav>
+            <nav  style={{fontSize: '12px' , marginRight : '10px' }}>About</nav>
+            <nav  style={{fontSize: '12px' , marginRight : '10px' }}>Contact</nav>
+
+            <button style={{width : '80px' , height : '40px' , backgroundColor : '#E52EE3' ,  color : 'white' , fontSize: '12px' , marginRight : '10px' }}>Register</button>
         </div>
     </div>
+   </div>
   )
 }
 
