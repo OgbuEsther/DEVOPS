1a6a5bb74e7d36a5100ec8462bb6b6b6d1005832
vangitech <softwareking101@gmail.com>
2025-03-04 10:08:54 +0100

    worked on the hero section again

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,37 +1,47 @@
 function Hero() {
   return (
-    <div style={{backgroundColor: "#150E28", color: "white", height: "100vh",}}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#150e28",
+        padding: "0 70px",
+        gap: "25px"
+      }}
+    >
       <div
         style={{
+          height: "50vh",
           display: "flex",
           justifyContent: "center",
-          flexDirection: "column",
-          position: "absolute",
-          top: "20%",
-          left: "40%",
-          padding: "0 2rem",
-        }}
-      >
-        <h1>Introduction To Get linked</h1>
-        <h2>tech Hackathon 1.0</h2>
-        <p>
-          Our tech hackathon is a melting pot of visionaries, and its purpose is
-          as clear as day: to shape the future. Whether you&apos;re a coding
-          genius, a design maverick, or a concept wizard, you&apos;ll have the
-          chance to transform your ideas into reality. Solving real-world
-          problems, pushing the boundaries of technology, and creating solutions
-          that can change the world, that&apos;s what we&apos;re all about!
-        </p>
-      </div>
-
-      <div
-        style={{
-          position: "relative",
-          top: "5%",
-          left: "0%",
+          alignItems: "center",
+          margin: "5px"
         }}
       >
         <img src="./bigbulb.png" alt="" />
+      </div>
+      <div
+        style={{
+          height: "50vh",
+          display: "block",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ textTransform: "capitalize", fontSize: "35px", color: "#fff", margin: "5px" }}>
+          Introduction to getlinked{" "}
+        </h1>
+        <h2 style={{ color: "#9c28bf", margin: "5px" }}>tech Hackathon 1.0</h2>
+        <p style={{ textAlign: "justify", color: "#fff", margin: "5px" }}>
+          Our tech hackathon is a melting pot of visionaries, and its purpose is
+          as clear as day: to shape the future. Whether you're a coding genius,
+          a design maverick, or a concept wizard, you'll have the chance to
+          transform your ideas into reality. Solving real-world problems,
+          pushing the boundaries of technology, and creating solutions that can
+          change the world, that's what we're all about!
+        </p>
       </div>
     </div>
   );
