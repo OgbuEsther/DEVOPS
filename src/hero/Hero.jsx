8bb6bd32a38b4c3acd55987a0598bba388e5d88b
vangitech <softwareking101@gmail.com>
2025-03-04 10:42:44 +0100

    worked on the hero section to align thext and image to be on same line

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -36,11 +36,11 @@ function Hero() {
         <h2 style={{ color: "#9c28bf", margin: "5px" }}>tech Hackathon 1.0</h2>
         <p style={{ textAlign: "justify", color: "#fff", margin: "5px" }}>
           Our tech hackathon is a melting pot of visionaries, and its purpose is
-          as clear as day: to shape the future. Whether you're a coding genius,
-          a design maverick, or a concept wizard, you'll have the chance to
+          as clear as day: to shape the future. Whether you&apos;re a coding genius,
+          a design maverick, or a concept wizard, you&apos;ll have the chance to
           transform your ideas into reality. Solving real-world problems,
           pushing the boundaries of technology, and creating solutions that can
-          change the world, that's what we're all about!
+          change the world, that&apos;s what we&apos;re all about!
         </p>
       </div>
     </div>
